623617b474a01689c958572eaea0809bd19ae50a
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:41:28 +0530

    Create Question-04.C

---
+++ Question-04.C
@@ -0,0 +1,14 @@
+// Write a program to print the first N odd natural numbers.
+
+#include<stdio.h>
+int main()
+{
+    int x , n ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    for(x=1 ; x<=n ; x+=2)
+        printf("%d\n",x);
+
+    return 0;
+}
