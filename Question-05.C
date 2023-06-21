b88ded207ffadf5ee0883180f9f491ff6cbacc19
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 19:55:58 +0530

    Update Question-05.C

--- Question-05.C
+++ Question-05.C
@@ -1,1 +1,17 @@
+// Write a program to print the first N odd natural numbers in reverse order.
 
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    while (n>=1)
+    {
+        printf("\n%d",2*n-1);
+        n--;
+
+    }
+    return 0;
+}
