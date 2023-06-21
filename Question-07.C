bc40fe1551edae1971ff4e63f24d236c1d1e61b3
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-21 20:08:37 +0530

    Update Question-07.C

--- Question-07.C
+++ Question-07.C
@@ -1,1 +1,16 @@
+// Write a program to print the first N even natural numbers in reverse order.
 
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+
+    while (n>=1)
+    {
+        printf("\n%d",2*n);
+        n--;
+    }
+    return 0;
+}
